4ac67c6c6527ed6dde73abbfe84ad3e780a291c6
Kyle Givler <6787154+JoyfulReaper@users.noreply.github.com>
2026-08-28 14:23:36 -0400

    Daytime client

---
+++ 03-daytime/daytime.c
@@ -0,0 +1,50 @@
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+    struct addrinfo hints, *result, *rp;
+    int error, sfd;
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+
+    error = getaddrinfo("daytime.kgivler.com", "13", &hints, &result);
+    if (error != 0)
+    {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(error));
+        return EXIT_FAILURE;
+    }
+
+    for (rp = result; rp != NULL; rp = rp->ai_next)
+    {
+        sfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+        if(sfd == -1)
+            continue;
+
+        if(connect(sfd, rp->ai_addr, rp->ai_addrlen) != -1)
+        {
+            break;
+        }
+
+        close(sfd);
+    }
+
+    freeaddrinfo(result);
+
+    ssize_t bytes_read = 0;
+    unsigned char buffer[4096];
+    while((bytes_read = read(sfd, buffer, sizeof(buffer))) > 0)
+    {
+        write(STDOUT_FILENO, buffer, bytes_read);
+    }
+
+    close(sfd);
+    return EXIT_SUCCESS;
+}
