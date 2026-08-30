f2f04493f49f6767af1f0ee51f3187e7256f439b
Kyle Givler <6787154+JoyfulReaper@users.noreply.github.com>
2026-08-30 14:56:55 -0400

    Inital tcp noise toy

--- 07-tcpnoise/src/main.c
+++ 07-tcpnoise/src/main.c
@@ -2,6 +2,12 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <string.h>
+
 void print_usage(char *program)
 {
 	fprintf(stderr, "Usage: %s <port>\n", program);
@@ -30,7 +36,63 @@ bool process_arguments (int argc, char *argv[], int *port)
 	return true;
 }
 
+int create_socket(void)
+{
+	int sfd;
+	
+	if ((sfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
+		perror("socket");
+		return -1;
+	}
+	
+	return sfd;
+}
 
+bool bind_socket(int sfd, int port)
+{
+	struct sockaddr_in address;
+	memset(&address, 0, sizeof address);
+	
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons(port);
+	
+	if(bind(sfd, (struct sockaddr*)&address, sizeof address) != 0)
+	{
+		perror("bind");
+		return false;
+	} 
+	
+	return true;
+}
+
+bool listen_socket(int sfd)
+{
+	if(listen(sfd, 5) != 0)
+	{
+		perror("listen");
+		return false;
+	}
+	
+	return true;
+}
+
+int accept_connection(int sfd)
+{
+	int cfd;
+	struct sockaddr_in address;
+	socklen_t peer_addr_size;
+	peer_addr_size = sizeof address;
+	
+	if( (cfd = accept(sfd, (struct sockaddr *)&address, &peer_addr_size)) < 0)
+	{
+		perror("accept");
+		return -1;
+	}
+	
+	return cfd;
+}
 
 int main (int argc, char *argv[])
 {
@@ -41,5 +103,38 @@ int main (int argc, char *argv[])
 		return EXIT_FAILURE;
 	}
 	
-	printf("Port: %d\n", port);
+	int sfd = create_socket();
+	if (sfd < 0)
+	{
+		return EXIT_FAILURE;
+	}
+	
+	if(!bind_socket(sfd, port))
+	{
+		close(sfd);
+		return EXIT_FAILURE;
+	}
+	
+	if(!listen_socket(sfd))
+	{
+		close(sfd);
+		return EXIT_FAILURE;
+	}
+	
+	printf("Listening for noise on port: %d\n", port);
+	
+	while (true) {
+		int cfd = accept_connection(sfd);
+		if(cfd < 0)
+		{
+			close(sfd);
+			return EXIT_FAILURE;
+		}
+		
+		printf("click!\n");
+		close(cfd);
+	}
+	
+	close(sfd);
+	return EXIT_SUCCESS;
 }
