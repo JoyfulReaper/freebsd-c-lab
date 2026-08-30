c869d30f106aaec7cc1d44ab0846764da5b167b0
Kyle Givler <6787154+JoyfulReaper@users.noreply.github.com>
2026-08-30 15:30:56 -0400

    Handle control-c

--- 07-tcpnoise/src/main.c
+++ 07-tcpnoise/src/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <errno.h>
 
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -8,6 +10,8 @@
 #include <netinet/in.h>
 #include <string.h>
 #include <arpa/inet.h>
+
+volatile sig_atomic_t running = 1;
 
 void print_usage(char *program)
 {
@@ -85,11 +89,24 @@ int accept_connection(int sfd, struct sockaddr_in *peer_addr)
 	socklen_t peer_addr_size = sizeof *peer_addr;
 	if((cfd = accept(sfd, (struct sockaddr *)peer_addr, &peer_addr_size)) < 0)
 	{
-		perror("accept");
-		return -1;
+		if(errno != EINTR)
+		{
+			perror("accept");
+			return -1;
+		}
+		else
+		{
+			return -1;
+		}
 	}
 	
 	return cfd;
+}
+
+void handle_sigint(int signal)
+{
+	(void)signal;
+	running = 0;
 }
 
 int main (int argc, char *argv[])
@@ -98,6 +115,17 @@ int main (int argc, char *argv[])
 	if(!process_arguments(argc, argv, &port))
 	{
 		print_usage(argv[0]);
+		return EXIT_FAILURE;
+	}
+	
+	struct sigaction action;
+	memset(&action, 0, sizeof action);
+	action.sa_handler = handle_sigint;
+	sigemptyset(&action.sa_mask);
+	
+	if(sigaction(SIGINT, &action, NULL) == -1)
+	{
+		fprintf(stderr, "Failed to register SIGINT signal handler");
 		return EXIT_FAILURE;
 	}
 	
@@ -123,10 +151,15 @@ int main (int argc, char *argv[])
 	
 	int connection_count = 0;
 	
-	while (true) {
+	while (running) {
 		struct sockaddr_in peer_addr;
 		int cfd = accept_connection(sfd,  &peer_addr);
-		if(cfd < 0)
+		if(cfd < 0 && errno == EINTR && running == 0)
+		{
+			printf("\nSIGINT caught, shutting down...\n");
+			break;
+		}
+		else if(cfd < 0)
 		{
 			close(sfd);
 			return EXIT_FAILURE;
@@ -139,6 +172,8 @@ int main (int argc, char *argv[])
 		printf("click! [%d] [remote: %s]\n", connection_count, ip);
 		close(cfd);
 	}
+	
+	printf("Connection attempts: %d\n", connection_count);
 	
 	close(sfd);
 	return EXIT_SUCCESS;
