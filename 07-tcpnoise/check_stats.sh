535394bc850bdb0c9096dc590aeb1ae1463124dd
Kyle Givler <6787154+JoyfulReaper@users.noreply.github.com>
2026-09-06 09:20:37 -0400

    Update check_stats

--- 07-tcpnoise/check_stats.sh
+++ 07-tcpnoise/check_stats.sh
@@ -1,24 +1,18 @@
+echo "Connection Statistics"
 sqlite3 -header -box tcpnoise.db 'SELECT port, ip_version, connection_count FROM port_activity ORDER BY connection_count DESC, port, ip_version'
-
-echo "Hall of shame"
-sqlite3 -header -box tcpnoise.db \
-'SELECT address, seen_count, first_seen_utc, last_seen_utc
- FROM seen_ip
- ORDER BY seen_count DESC
- LIMIT 1;'
-
-echo "10 most seen"
-sqlite3 -header -box tcpnoise.db \
-'SELECT address, seen_count, first_seen_utc, last_seen_utc
- FROM seen_ip
- ORDER BY seen_count DESC
- LIMIT 10;'
 
 echo "10 most recently seen"
 sqlite3 -header -box tcpnoise.db \
 'SELECT address, seen_count, first_seen_utc, last_seen_utc
  FROM seen_ip
  ORDER BY last_seen_utc DESC
+ LIMIT 10;'
+
+echo "10 most seen"
+sqlite3 -header -box tcpnoise.db \
+'SELECT address, seen_count, first_seen_utc, last_seen_utc
+ FROM seen_ip
+ ORDER BY seen_count DESC
  LIMIT 10;'
 
 echo "Any IPv6?"
