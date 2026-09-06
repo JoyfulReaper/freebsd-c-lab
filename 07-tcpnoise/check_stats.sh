319b2cfb33ce054f06c2697739c4fb6a3928806a
Kyle Givler <6787154+JoyfulReaper@users.noreply.github.com>
2026-09-05 22:41:38 -0400

    Update check_stats.sh

--- 07-tcpnoise/check_stats.sh
+++ 07-tcpnoise/check_stats.sh
@@ -1,2 +1,4 @@
-sqlite3 tcpnoise.db 'SELECT port, ip_version, connection_count FROM port_activity ORDER BY port, ip_version;'
+sqlite3 -header -box tcpnoise.db 'SELECT port, ip_version, connection_count FROM port_activity ORDER BY connection_count DESC, port, ip_version'
 
+echo "Any IPv6?"
+grep IPv6 *.log | tail
